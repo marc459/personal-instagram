087486af60bec877cc4d85fdbdb516ba97317e08
dalexhd <eldalexhd@gmail.com>
2021-08-21 20:32:54 +0200

    feat: add cli.

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,30 @@ instagram.on('loggedIn', async () => {
   logger.info(
     `Successfully logged in with ${instagram.user.username}'s account!`
   );
-  logger.debug('Generating friday profile picture...');
-  const fridayProfilePic = await instagram.generateFridayProfilePic(
-    instagram.user.username
-  );
-  try {
-    await instagram.ig.account.changeProfilePicture(fridayProfilePic);
-    logger.info(`Successfully changed to the friday's avatar!`);
-  } catch (error) {
-    logger.error(error.message);
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    switch (arg) {
+      case '--set-friday-pic':
+        logger.debug('Generating friday profile picture...');
+        const fridayProfilePic = await instagram.generateFridayProfilePic(
+          instagram.user.username
+        );
+        try {
+          await instagram.ig.account.changeProfilePicture(fridayProfilePic);
+          logger.info(`Successfully changed to the friday's avatar!`);
+        } catch (error) {
+          logger.error(error.message);
+        }
+        break;
+      case '--a':
+        console.log('a');
+        break;
+      case '--b':
+        console.log('b');
+        break;
+      default:
+        break;
+    }
   }
 });
 
